7f213461ef9317f3a61fdcf81c486a85f765ec13
fahmi171212121 <141645642+fahmi171212121@users.noreply.github.com>
2024-11-02 10:35:29 +0700

    Update BMIberaksi.java

--- src/bmi/BMIberaksi.java
+++ src/bmi/BMIberaksi.java
@@ -12,17 +12,16 @@ package bmi;
 public class BMIberaksi {
 
     public static void main(String[] args) {
-        // Misalkan kita ingin menghitung BMI untuk berat 70 kg dan tinggi 1.75 m
+
         double berat = 40; // berat dalam kg
         double tinggiM = 1.60; // tinggi dalam m
 
-        // Membuat objek BMI
+
         BMI bmiCalculator = new BMI();
 
-        // Menghitung BMI
         bmiCalculator.hitungBMI(berat, tinggiM);
 
-        // Menampilkan hasil
+
         System.out.printf("BMI Anda adalah: %.2f\n", bmiCalculator.getBMI());
         System.out.println("Kategori: " + bmiCalculator.getKategori());
     }
